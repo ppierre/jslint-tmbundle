f5efc599f81ef03c6cf00d3bdad6b454ffc8b0db
Pierre Pracht <pierre.pracht@gmail.com>
2009-02-22 00:33:20 +0100

    closure

--- Support/jslint_textmate.js
+++ Support/jslint_textmate.js
@@ -29,40 +29,42 @@ SOFTWARE.
 
 "use strict";
 
-var old_nclick = nclick;
+(function () {
+  var old_nclick = nclick,
+      ns = document.getElementsByName('jslint'),
+      n, i;
 
-nclick = function () {
-  old_nclick();
-  var results, i, result, text, new_text;
-  results = document.evaluate("//div[@id='output']//p[not(@class='evidence')]", document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
-  for (i = 0; i < results.snapshotLength; i += 1) {
-    result = results.snapshotItem(i);
-    text = result.innerHTML;
-    new_text = text.replace(
-      /line (\d+) character (\d+)/,
-      '<a href="txmt://open/?url=file://' +
-      file_name +
-      '&line=$1&column=$2">line $1 character $2</a>');
-    result.innerHTML = new_text;
-  }
-  results = document.evaluate("//div[@id='functions']//div[@class='function']/i[1]", document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
-  for (i = 0; i < results.snapshotLength; i += 1) {
-    result = results.snapshotItem(i);
-    text = result.innerHTML;
-    new_text = text.replace(
-      /(\d+)/,
-      '<a href="txmt://open/?url=file://' +
-      file_name +
-      '&line=$1">line $1</a>');
-    result.innerHTML = new_text;
-  }
-};
+  nclick = function () {
+    old_nclick();
+    var results, i, result, text, new_text;
+    results = document.evaluate("//div[@id='output']//p[not(@class='evidence')]", document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+    for (i = 0; i < results.snapshotLength; i += 1) {
+      result = results.snapshotItem(i);
+      text = result.innerHTML;
+      new_text = text.replace(
+        /line (\d+) character (\d+)/,
+        '<a href="txmt://open/?url=file://' +
+        file_name +
+        '&line=$1&column=$2">line $1 character $2</a>');
+      result.innerHTML = new_text;
+    }
+    results = document.evaluate("//div[@id='functions']//div[@class='function']/i[1]", document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+    for (i = 0; i < results.snapshotLength; i += 1) {
+      result = results.snapshotItem(i);
+      text = result.innerHTML;
+      new_text = text.replace(
+        /(\d+)/,
+        '<a href="txmt://open/?url=file://' +
+        file_name +
+        '&line=$1">line $1</a>');
+      result.innerHTML = new_text;
+    }
+  };
 
-var ns = document.getElementsByName('jslint');
-
-for (var i = 0; i < ns.length; i += 1) {
-  var n = ns[i];
-  if (n.value === 'JSLint') {
-    n.onclick = nclick;
+  for (i = 0; i < ns.length; i += 1) {
+    n = ns[i];
+    if (n.value === 'JSLint') {
+      n.onclick = nclick;
+    }
   }
-}
+})();
